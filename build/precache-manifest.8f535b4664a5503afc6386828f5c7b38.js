8a876513d154f68e73ac70140fbaa13b50d326dc
joao.barreto <joao.barreto@itbam.org.br>
2021-01-19 22:20:08 -0400

    build

---
+++ build/precache-manifest.8f535b4664a5503afc6386828f5c7b38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a032b4fa6db1cdd7896bd4a5f6fbab0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded1ccd776d4d4175aaa",
+    "url": "/static/css/main.739cab1a.chunk.css"
+  },
+  {
+    "revision": "18fa889d4e0b5bdd2c59",
+    "url": "/static/js/2.4d3d53d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d3d53d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded1ccd776d4d4175aaa",
+    "url": "/static/js/main.65778c0d.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "80e9b312fd92cc913ca3688a51b5c8c2",
+    "url": "/static/media/back.80e9b312.svg"
+  },
+  {
+    "revision": "dbd9a29d7281bb8792d338db6bc463c7",
+    "url": "/static/media/delete.dbd9a29d.svg"
+  },
+  {
+    "revision": "be75cf84f03b8afe4a3c674fccb9cc45",
+    "url": "/static/media/handshake.be75cf84.svg"
+  },
+  {
+    "revision": "5f50f59f291d08bea0fd30ef11026f1e",
+    "url": "/static/media/purple-heart.5f50f59f.svg"
+  },
+  {
+    "revision": "a9171846f4d04b996247ed72c60c4d3a",
+    "url": "/static/media/sos.a9171846.svg"
+  },
+  {
+    "revision": "1b8b6ec316ea67dcfb34aacd624b8866",
+    "url": "/static/media/warning.1b8b6ec3.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
